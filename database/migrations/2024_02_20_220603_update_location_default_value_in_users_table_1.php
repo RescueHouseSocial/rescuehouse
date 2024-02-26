cba9b27dfb0dcec13fca541842f7117373a3d9ae
Schroeder <schroeder@Schroeders-Mac-mini.local>
2024-02-26 12:40:20 -0600

    update

--- database/migrations/2024_02_20_220603_update_location_default_value_in_users_table_1.php
+++ database/migrations/2024_02_20_220603_update_location_default_value_in_users_table_1.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->longText('location')->nullable()->default('')->change();
-            $table->text('biography')->nullable()->default('')->change();
+            $table->longText('location')->nullable()->change();
+            $table->text('biography')->nullable()->change();
         });
     }
 
