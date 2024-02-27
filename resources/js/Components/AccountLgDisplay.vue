18ac2690fade45f7903f767ddc6a3b59a58def69
Schroeder <schroeder@Schroeders-Mac-mini.local>
2024-02-26 19:03:40 -0600

    update

--- resources/js/Components/AccountLgDisplay.vue
+++ resources/js/Components/AccountLgDisplay.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="AccountLgDisplay">
-    <div class="items-center bg-gray-50 rounded-lg shadow sm:flex">
+    <div class="bg-gray-50 rounded-lg shadow sm:flex">
       <div v-if="avatar" class="w-100 h-100">
         <img :src="`/storage/avatars/medium/${avatar.path}`" class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" alt="user avatar"/>
       </div>
-      <div class="p-4">
-        <h3 class="text-xl font-bold tracking-tight text-gray-900">{{ users.name }}</h3>
-        <span class="text-gray-500">Location</span>
-        <p class="mt-4 mb-4 font-light text-gray-500">Biography</p>
+      <div class="flex flex-col w-full">
+        <div class="flex justify-center p-4">
+          <button 
+            @click="handleFollowToggle"
+            :class="{'border-2 border-gray-600': isFollowing, 'flex mx-auto text-white bg-orange-500 border-2 border-transparent py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg': true}"
+          >{{ buttonText }}</button>
+        </div>
+        <div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900">{{ users.name }}</h3>
+          <span class="text-gray-500">Location</span>
+          <p class="mt-4 mb-4 font-light text-gray-500">Biography</p>
+        </div>
       </div>
     </div>
   </div>
@@ -15,6 +23,20 @@
 
 <script setup>
 
+  import { ref } from 'vue';
+  let buttonText = ref('Follow');
+  let isFollowing = ref(false);
+
   const props = defineProps(["users", "avatar"]);
+
+  const handleFollowToggle = () => {
+    if (isFollowing.value === false) {
+      buttonText.value = 'Following';
+      isFollowing.value = true;
+    } else {
+      buttonText.value = 'Follow';
+      isFollowing.value = false;
+    }
+  };
 
 </script>
