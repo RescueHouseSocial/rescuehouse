6d237f4f5150e0444a6123ea472755c943b266d3
Benjamin Schroeder <yozomo@gmail.com>
2024-03-31 20:10:19 -0500

    update

--- resources/js/Components/DynamicCarousel.vue
+++ resources/js/Components/DynamicCarousel.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="DynamicCarousel">
     <div v-if="galleries && galleries.length > 0">
-      <div class="flex w-full overflow-hidden rounded justify-center">
+      <div class="flex justify-center">
+      <div class="flex w-1/2 overflow-hidden rounded">
         <div class="inner" ref="inner" :style="innerStyles">
           <div v-for="image in galleries" :key="image.id" class="inline-flex justify-center">
             <img :src="`/storage/gallery/medium/${image.path}`" class="rounded" alt="Gallery Image"/>
           </div>
         </div>
+      </div>
       </div>
       <button @click="prev" class="hover:text-orange-400"><i class="fa-solid fa-arrow-left fa-fw"></i></button>
       <button @click="next" class="hover:text-orange-400"><i class="fa-solid fa-arrow-right fa-fw"></i></button>
@@ -27,70 +29,70 @@ export default {
       transitioning: false,
     }
   },
-  // mounted () {
-  //   this.setStep();
-  //   this.resetTranslate();
-  // },
-  // methods: {
-  //   setStep () {
-  //     if (this.galleries && this.galleries.length > 0) {
-  //       const innerWidth = this.$refs.inner.scrollWidth;
-  //       const totalCards = this.galleries.length;
-  //       this.step = `${innerWidth / totalCards}px`;
-  //     }
-  //   },
+  mounted () {
+    this.setStep();
+    this.resetTranslate();
+  },
+  methods: {
+    setStep () {
+      if (this.galleries && this.galleries.length > 0) {
+        const innerWidth = this.$refs.inner.scrollWidth;
+        const totalCards = this.galleries.length;
+        this.step = `${innerWidth / totalCards}px`;
+      }
+    },
 
-  //   next () {
-  //     if (this.transitioning) return;
-  //     this.transitioning = true;
-  //     this.moveLeft();
-  //     this.afterTransition(() => {
-  //       const card = this.galleries.shift();
-  //       this.galleries.push(card);
-  //       this.resetTranslate();
-  //       this.transitioning = false;
-  //     });
-  //   },
+    next () {
+      if (this.transitioning) return;
+      this.transitioning = true;
+      this.moveLeft();
+      this.afterTransition(() => {
+        const card = this.galleries.shift();
+        this.galleries.push(card);
+        this.resetTranslate();
+        this.transitioning = false;
+      });
+    },
 
-  //   prev () {
-  //     if (this.transitioning) return;
-  //     this.transitioning = true;
-  //     this.moveRight();
-  //     this.afterTransition(() => {
-  //       const card = this.galleries.pop();
-  //       this.galleries.unshift(card);
-  //       this.resetTranslate();
-  //       this.transitioning = false;
-  //     });
-  //   },
+    prev () {
+      if (this.transitioning) return;
+      this.transitioning = true;
+      this.moveRight();
+      this.afterTransition(() => {
+        const card = this.galleries.pop();
+        this.galleries.unshift(card);
+        this.resetTranslate();
+        this.transitioning = false;
+      });
+    },
 
-  //   moveLeft () {
-  //     this.innerStyles = {
-  //       transform: `translateX(-${this.step}) translateX(-${this.step})`
-  //     }
-  //   },
+    moveLeft () {
+      this.innerStyles = {
+        transform: `translateX(-${this.step}) translateX(-${this.step})`
+      }
+    },
 
-  //   moveRight () {
-  //     this.innerStyles = {
-  //       transform: `translateX(${this.step}) translateX(-${this.step})`
-  //     }
-  //   },
+    moveRight () {
+      this.innerStyles = {
+        transform: `translateX(${this.step}) translateX(-${this.step})`
+      }
+    },
 
-  //   afterTransition (callback) {
-  //     const listener = () => {
-  //       callback();
-  //       this.$refs.inner.removeEventListener('transitionend', listener);
-  //     };
-  //     this.$refs.inner.addEventListener('transitionend', listener);
-  //   },
+    afterTransition (callback) {
+      const listener = () => {
+        callback();
+        this.$refs.inner.removeEventListener('transitionend', listener);
+      };
+      this.$refs.inner.addEventListener('transitionend', listener);
+    },
 
-  //   resetTranslate () {
-  //     this.innerStyles = {
-  //       transition: 'none',
-  //       transform: `translateX(-${this.step})`
-  //     };
-  //   }
-  // }
+    resetTranslate () {
+      this.innerStyles = {
+        transition: 'none',
+        transform: `translateX(-${this.step})`
+      };
+    }
+  }
 
 };
 
